49b1371811abe6f85a8db258de9e15de0fefbd64
Sanooj Es <sanoojes6371@gmail.com>
2024-09-24 23:45:26 +0530

    :bug: fix: other page header

--- extension/src/utils/pathUtils.ts
+++ extension/src/utils/pathUtils.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLucidStore } from '@/store/useLucidStore';
 import { useBodyClass } from '@/hooks/useBodyClass';
 
-export const getPathCategory = (pathname: string) => {
+export const getPathCategory = (pathname: string): PageCategoryType => {
   if (Spicetify.URI.isPlaylistV1OrV2(pathname)) return 'playlist';
   if (Spicetify.URI.isArtist(pathname)) return 'artist';
   if (Spicetify.URI.isAlbum(pathname)) return 'album';
