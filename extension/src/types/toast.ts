bfd41ed9f24e277db54e99c1b5476945ac006044
Sanooj Es <sanoojes6371@gmail.com>
2024-10-23 21:39:03 +0530

    :sparkles: feat: add toast type

--- extension/src/types/toast.ts
+++ extension/src/types/toast.ts
@@ -1,7 +1,9 @@
+import { ReactNode } from "react";
+
 export type ToastType = {
-	id: number;
-	message: string;
-	exiting: boolean;
-	isError: boolean;
+  id: number;
+  message: ReactNode;
+  exiting: boolean;
+  isError: boolean;
 };
-export type ToastContextType = (message: string) => void;
+export type ToastContextType = (message: ReactNode) => void;
