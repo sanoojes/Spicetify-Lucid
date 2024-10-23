e4565f8b6d29c42acc0adb85cd870801d0bf8158
Sanooj Es <sanoojes6371@gmail.com>
2024-10-23 12:28:36 +0530

    :sparkles: feat: add changelogs

--- extension/src/components/Main.tsx
+++ extension/src/components/Main.tsx
@@ -7,36 +7,44 @@ import { manageBackgroundZIndex } from "@/utils/backgroundUtils";
 import { logDebug } from "@/utils/logUtils";
 import { replaceIcons } from "@/utils/replaceIcons";
 import React, { useEffect } from "react";
+import ChangeLogManager from "./changelog/ChangeLogManager";
 
 /**
  * Renders Main things for the theme
  */
 const Main = () => {
-	logDebug("Render <Main />");
+  logDebug("Render <Main />");
 
-	useEffect(() => {
-		replaceIcons();
-		manageBackgroundZIndex();
-	}, []);
+  useEffect(() => {
+    replaceIcons();
+    manageBackgroundZIndex();
+  }, []);
 
-	return (
-		<>
-			<div id="state">
-				<MainStateManager />
-			</div>
-			<div id="background-container" className="background-container" style={{ containerType: "normal" }}>
-				<BackgroundManager />
-			</div>
-			<ModalContextProvider>
-				<div id="modal-container" className="modal-container" style={{ containerType: "normal" }}>
-					<SettingsManager />
-					{/* <ChangeLogManager /> */}
-				</div>
-			</ModalContextProvider>
-			<div>{/* <WindowControlsManager /> */}</div>
-			<ToastContainer />
-		</>
-	);
+  return (
+    <>
+      <div
+        id='background-container'
+        className='background-container'
+        style={{ containerType: "normal" }}
+      >
+        <BackgroundManager />
+      </div>
+      <ModalContextProvider>
+        <div
+          id='modal-container'
+          className='modal-container'
+          style={{ containerType: "normal" }}
+        >
+          <SettingsManager />
+          <ChangeLogManager />
+        </div>
+      </ModalContextProvider>
+      <div id='state'>
+        <MainStateManager />
+      </div>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default Main;
