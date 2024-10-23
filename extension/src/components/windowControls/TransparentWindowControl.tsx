890cf8bc71cf4938ec98f4fab4ae4c0286764f89
Sanooj Es <sanoojes6371@gmail.com>
2024-10-23 09:53:59 +0530

    :bug: fix: transparent window controls top for <=1.2.45

--- extension/src/components/windowControls/TransparentWindowControl.tsx
+++ extension/src/components/windowControls/TransparentWindowControl.tsx
@@ -1,63 +1,72 @@
-import { isCustomControls, isLightModeEnabled, isSpotifyV16Above, isWindowsPlatform } from "@/constants/constants";
+import {
+  isCustomControls,
+  isLightModeEnabled,
+  isSpotifyV16Above,
+  isWindowsPlatform,
+} from "@/constants/constants";
 import { useLucidStore } from "@/store/useLucidStore";
 import { useSettingsStore } from "@/store/useSettingsStore";
 import { setWindowControlsHeight } from "@/utils/windowControlUtils";
 import React, { useEffect, useRef, useState, type CSSProperties } from "react";
 
 const TransparentWindowControl = () => {
-	const TransparentWindowControlRef = useRef<HTMLDivElement | null>(null);
-	const [style, setStyle] = useState<CSSProperties>({});
-	const { windowZoom } = useLucidStore();
-	const {
-		interfaceSettings: {
-			controlSettings: { height: controlHeight },
-		},
-	} = useSettingsStore();
+  const TransparentWindowControlRef = useRef<HTMLDivElement | null>(null);
+  const [style, setStyle] = useState<CSSProperties>({});
+  const { windowZoom } = useLucidStore();
+  const {
+    interfaceSettings: {
+      controlSettings: { height: controlHeight },
+    },
+  } = useSettingsStore();
 
-	useEffect(() => {
-		const setTopBarStyles = () => {
-			if (isWindowsPlatform) {
-				setWindowControlsHeight(controlHeight);
+  useEffect(() => {
+    const setTopBarStyles = () => {
+      if (isWindowsPlatform) {
+        setWindowControlsHeight(controlHeight);
 
-				if (!isCustomControls && !isLightModeEnabled) {
-					const normalHeight = isSpotifyV16Above ? 32 : 64;
-					const height = normalHeight / windowZoom;
-					const controlTop = (controlHeight / windowZoom - Math.min(32 / windowZoom, controlHeight / windowZoom)) / 2;
+        if (!isCustomControls && !isLightModeEnabled) {
+          const normalHeight = isSpotifyV16Above ? 32 : 64;
+          const height = normalHeight / windowZoom;
+          const controlTop = isSpotifyV16Above
+            ? (controlHeight / windowZoom -
+                Math.min(32 / windowZoom, controlHeight / windowZoom)) /
+              2
+            : 0;
 
-					const newStyle: CSSProperties = {
-						position: "fixed",
-						right: 0,
-						top: `${controlTop}px`,
-						height: `${height}px`,
-						width: `${135 / windowZoom}px`,
-						WebkitBackdropFilter: "brightness(2.12)",
-						backdropFilter: "brightness(2.12)",
-						zIndex: "var(--above-main-and-now-playing-view-grid-area, 6)",
-					};
+          const newStyle: CSSProperties = {
+            position: "fixed",
+            right: 0,
+            top: `${controlTop}px`,
+            height: `${height}px`,
+            width: `${135 / windowZoom}px`,
+            WebkitBackdropFilter: "brightness(2.12)",
+            backdropFilter: "brightness(2.12)",
+            zIndex: "var(--above-main-and-now-playing-view-grid-area, 6)",
+          };
 
-					setStyle(newStyle);
-				} else {
-					setStyle({}); //  remove styles
-				}
-			}
-		};
+          setStyle(newStyle);
+        } else {
+          setStyle({}); //  remove styles
+        }
+      }
+    };
 
-		setTopBarStyles();
+    setTopBarStyles();
 
-		const handleResize = () => {
-			setTopBarStyles();
-		};
+    const handleResize = () => {
+      setTopBarStyles();
+    };
 
-		const timeoutId = setTimeout(setTopBarStyles, 1000);
-		window.addEventListener("resize", handleResize);
+    const timeoutId = setTimeout(setTopBarStyles, 1000);
+    window.addEventListener("resize", handleResize);
 
-		return () => {
-			clearTimeout(timeoutId);
-			window.removeEventListener("resize", handleResize);
-		};
-	}, [windowZoom, controlHeight]);
+    return () => {
+      clearTimeout(timeoutId);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowZoom, controlHeight]);
 
-	return <div ref={TransparentWindowControlRef} style={style} />;
+  return <div ref={TransparentWindowControlRef} style={style} />;
 };
 
 export default TransparentWindowControl;
