08ed06fdb3d40d99ed01b160c0d65072365d6e6e
Sanooj Es <sanoojes6371@gmail.com>
2024-10-30 09:22:32 +0530

    :sparkles: feat: update links from latest to ref

--- remote/theme.js
+++ remote/theme.js
@@ -1,6 +1,7 @@
 (() => {
-	const themeScript = document.createElement("script");
-	themeScript.type = "text/javascript";
-	themeScript.src = "https://cdn.jsdelivr.net/gh/sanoojes/Spicetify-Lucid@latest/src/theme.js";
-	document.head.appendChild(themeScript);
+  const themeScript = document.createElement("script");
+  themeScript.type = "text/javascript";
+  themeScript.src =
+    "https://cdn.jsdelivr.net/gh/sanoojes/Spicetify-Lucid@refs/heads/main/src/theme.js";
+  document.head.appendChild(themeScript);
 })();
