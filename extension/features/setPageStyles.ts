4cba67180ba7db6ef5b522a63ae7468b8586ab93
Sanooj Es <sanoojes6371@gmail.com>
2025-08-10 14:15:41 +0530

    feat: add playlist modal styles

--- extension/features/setPageStyles.ts
+++ extension/features/setPageStyles.ts
@@ -1,4 +1,7 @@
 import appStore from '@store/appStore.ts';
+import waitForElements from '@utils/dom/waitForElements.ts';
+import { updateCardBgAlpha } from '@utils/updateCardBgAlpha.ts';
+import debounce from '@utils/debounce.ts';
 
 export default function setPageStyles(page = appStore.getState().page) {
   const { coverMode, homeCardGap, mode, panelGap } = page;
@@ -17,4 +20,27 @@ export default function setPageStyles(page = appStore.getState().page) {
   document.body.style.setProperty('--panel-gap', `${panelGap}px`);
 }
 
+const observeForPlaylistModal = () => {
+  waitForElements('.Root').then((rootElem) => {
+    const rootObserver = new MutationObserver(
+      debounce(() => {
+        const sourceDiv = rootElem.querySelector('.FP_XXx0FMQPJEu3WzfpM') as HTMLDivElement | null;
+        if (!sourceDiv) return;
+
+        const styleObserver = new MutationObserver(() => {
+          const rgbRegex = /rgb\(\s*((?:\d{1,3}\s*,\s*){2}\d{1,3})\s*\)/;
+          const color = sourceDiv.style.backgroundColor.match(rgbRegex)?.[1];
+          if (!color || !sourceDiv.parentElement) return;
+
+          sourceDiv.parentElement.style.setProperty('--accent-color', color);
+        });
+
+        styleObserver.observe(sourceDiv, { attributes: true });
+      }, 300)
+    );
+    rootObserver.observe(rootElem, { childList: true });
+  });
+};
+
+observeForPlaylistModal();
 appStore.subscribe((state) => state.page, setPageStyles);
