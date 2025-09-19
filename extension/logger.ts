a52a84cd87fa7aeee3d3cf47a6c4040bb7d61160
Sanooj Es <sanoojes6371@gmail.com>
2025-09-19 12:34:49 +0530

    fix: logger prefix

--- extension/logger.ts
+++ extension/logger.ts
@@ -1,9 +1,9 @@
 export const createLogger = (prefix: string = '') => ({
-  info: (...args: any[]) => console.log('[Lucid-Lyrics]', prefix, ...args),
-  debug: (...args: any[]) => console.debug('[Lucid-Lyrics]', prefix, ...args),
-  warn: (...args: any[]) => console.trace('[Lucid-Lyrics]', prefix, ...args),
-  trace: (...args: any[]) => console.trace('[Lucid-Lyrics]', prefix, ...args),
-  error: (...args: any[]) => console.error('[Lucid-Lyrics]', prefix, ...args),
+  info: (...args: any[]) => console.log('[Lucid]', prefix, ...args),
+  debug: (...args: any[]) => console.debug('[Lucid]', prefix, ...args),
+  warn: (...args: any[]) => console.trace('[Lucid]', prefix, ...args),
+  trace: (...args: any[]) => console.trace('[Lucid]', prefix, ...args),
+  error: (...args: any[]) => console.error('[Lucid]', prefix, ...args),
 });
 
 export const logger = createLogger();
